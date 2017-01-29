696d466d2fae76ec7fa5400a204481c4d3123da7
Rodric Rabbah <rabbah@us.ibm.com>
2017-01-29 02:31:24 -0500

    Serverless UI to generate QR using OpenWhisk web actions.
    Add simple UI for QR generator.
    Use OpenWhisk web actions.
    Update README.
    Add helper script to deploy/teardown.
    Add action to retrieve UI host URL.

--- src/main/java/qr/Generate.java
+++ src/main/java/qr/Generate.java
@@ -1,19 +1,21 @@
 package qr;
 
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.Base64;
 
 import com.google.gson.JsonObject;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.*;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
 public class Generate {
   public static JsonObject main(JsonObject args) throws Exception {
-    String text = args.getAsJsonPrimitive("text").getAsString();
+    String property = "text";
+    String text = "Hello. Try with a 'text' value next time.";
+    if (args.has(property)) {
+      text = args.get(property).toString();
+    }
 
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     OutputStream b64os = Base64.getEncoder().wrap(baos);    
@@ -25,7 +27,12 @@ public class Generate {
     String output = baos.toString("utf-8");
 
     JsonObject response = new JsonObject();
-    response.addProperty("qr", output);
+    JsonObject headers = new JsonObject();
+    headers.addProperty("content-type", "image/png");
+    response.add("headers", headers);
+    response.addProperty("body", output);
     return response;
   }
 }
+
+
