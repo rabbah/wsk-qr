dab3fa73ffd2395f0e43c5ee3d249e67388ea9b5
Carlos Santana <csantana23@gmail.com>
2017-08-30 21:06:24 -0400

    remove experimental web endpoint (#2)

--- src/main/js/host.js
+++ src/main/js/host.js
@@ -1,7 +1,7 @@
 function main(args) {
     let host = process.env['__OW_API_HOST']
     let ns = process.env['__OW_NAMESPACE']
-    let base = `${host}/api/v1/experimental/web/${ns}`
+    let base = `${host}/api/v1/web/${ns}`
     if (args.path) {
         return { url: `${base}/${args.path}` }
     } else return { url: base }
